f88dd571d6fa39005b467fe2e5bd20fd693fb23d
Lyra Hall <lyrahall14@gmail.com>
2018-01-23 21:00:17 +0000

    sent a text to twilio and twilio texted me back! aka got webhook to work!

--- server.py
+++ server.py
@@ -1,9 +1,27 @@
-from flask import Flask
+# /usr/bin/env python
+# Download the twilio-python library from twilio.com/docs/libraries/python
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
 app = Flask(__name__)
 
+app.secret_key = "hostesswiththemostest"
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+	return "Hello, World!"
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    # Start our response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("Ahoy! Thanks so much for your message.")
+
+    return str(resp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+	app.debug = True
+	app.run(port=5000, host="0.0.0.0")
